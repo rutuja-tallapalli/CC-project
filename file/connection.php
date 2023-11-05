88b5f443193e676a9b6a93bb90dbd0e3bc717bd0
Ajaykumar Shendage <143033613+VersatileSoul@users.noreply.github.com>
2023-11-06 02:46:00 +0530

    Update connection.php

--- file/connection.php
+++ file/connection.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = "database-1.cmt1ygketgpr.us-east-1.rds.amazonaws.com";
+$username = "admin";
+$password = "admin123";
 $dbname = "bloodbank";
 $conn = new mysqli($servername, $username, $password, $dbname);
 if(!$conn){
